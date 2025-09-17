a4771621e277ce6ef55dd3df2f9c4092592e9078
Tiago Burlamaque <tiagoburlamaque79@gmail.com>
2025-09-17 11:42:52 -0300

    Add files via upload

---
+++ codigo.py
@@ -0,0 +1,37 @@
+import pyautogui
+
+# Dica: Sempre começar com o código em português, como Exemplo:
+
+# passo 1: Entar no sistema da empresa https://dlp.hashtagtreinamentos.com/python/intensivao/tabela
+# Abrir o janelinha inicial do windows
+pyautogui.press("win")
+# Digitar Google Chrome 
+pyautogui.write("Google Chrome")
+# Entrar no Google Chrome
+pyautogui.press("enter")
+
+pyautogui.hotkey("win", "r")
+pyautogui.write("cmd")
+pyautogui.press("enter")
+pyautogui.write("git add .")
+pyautogui.write('git commit -m "Automatizando projeto"')
+pyautogui.write("git push")
+
+
+
+# passo 2: Fazer Login
+# passo 3: Importar a base de dados
+# passo 4: Cadastrar 1 produto
+# passo 5: Repetir para todos os produtos
+
+# pyautogui -> Fazer automações com python
+
+# baixar pacote no cmd -> pip install pyautogui
+
+# -----------------------------------------------------------------------------------------------------
+
+# pyautogui.click -> clicar em algum lugar
+# pyautogui.press -> apertar uma tecla do teclado
+# pyautogui.write -> escrever um texto
+# pyautogui.hotkey -> apertar uma combinação de teclas(teclas de atalhos)
+
