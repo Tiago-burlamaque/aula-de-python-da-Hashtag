8e50afa454308e8449e0b36f0ba699f273c99571
TIAGO BURLAMAQUE SOUSA FILHO <tiago_sousa-filho@estudante.sesisenai.org.br>
2025-09-17 18:36:04 -0300

    Automatizando projeto

--- atividades de curiosidade/automatizarGitPush.py
+++ atividades de curiosidade/automatizarGitPush.py
@@ -1,11 +1,14 @@
 import pyautogui
 
-# Eu quero abrir o terminal
-pyautogui.hotkey("Ctrl", "'")
-# Eu quero adicionar o projeto no github
-pyautogui.write("git add .")
-pyautogui.press("enter")
-pyautogui.write('git commit -m "Automatizando projeto"')
-pyautogui.press('enter')
-pyautogui.write("git push")
-pyautogui.press("enter")
+def gitPush():
+    # Eu quero abrir o terminal
+    pyautogui.hotkey("Ctrl", "'")
+    # Eu quero adicionar o projeto no github
+    pyautogui.write("git add .")
+    pyautogui.press("enter")
+    pyautogui.write('git commit -m "Automatizando projeto"')
+    pyautogui.press('enter')
+    pyautogui.write("git push")
+    pyautogui.press("enter")
+
+gitPush()
--- codigo.py
+++ codigo.py
@@ -1,4 +1,7 @@
 import pyautogui
+import time
+
+pyautogui.PAUSE = 0.8
 
 # Dica: Sempre começar com o código em português, como Exemplo:
 
@@ -6,15 +9,26 @@ import pyautogui
 # Abrir o janelinha inicial do windows
 pyautogui.press("win")
 # Digitar Google Chrome 
-pyautogui.write("Google Chrome")
+pyautogui.write("chrome")
 # Entrar no Google Chrome
+pyautogui.press("enter")
+# Digitar o site
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+# Entrar no site
+pyautogui.press("enter")
+
+# espera 3 segundos
+time.sleep(3)
+
+# passo 2: Fazer Login
+pyautogui.click(x=489, y=428)
+pyautogui.write("Autismo@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("Autismo123")
+pyautogui.press("tab")
 pyautogui.press("enter")
 
 
-
-
-
-# passo 2: Fazer Login
 # passo 3: Importar a base de dados
 # passo 4: Cadastrar 1 produto
 # passo 5: Repetir para todos os produtos
